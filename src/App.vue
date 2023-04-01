241b579c99f04cdfb2843f55cd5a19ffa48bf755
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-01 17:26:48 +0900

    一旦保存

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, onUnmounted,nextTick, reactive } from 'vue';
+import SearchField from './components/SearchField.vue';
+import TagList from './components/TagList.vue';
+
+let lang = "ja"
+
+let messages = reactive({
+  text: "hello"
+})
+
+// Intl.DateTimeFormat().resolvedOptions().timeZone
+// window.navigator.language
+
+onMounted(() => {
+  lang = (window.navigator.language).substring(0,2)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <v-app>
+    <SearchField :lang="lang"/>
+    <TagList 
+      :lang="lang"
+      :tag-list="[{id:1,name:'aaa'},{id:2,name:'bbb'},{id:3,name:'ccc'}]"
+    />
+  </v-app>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
