79fe7c1e5675645716657b35a24d7cb95b24a5c5
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-03 00:49:58 +0900

    トークンをローカルストレージに保存､削除できるようにした

--- src/store.js
+++ src/store.js
@@ -3,20 +3,29 @@ import { createStore } from 'vuex'
 export default createStore({
 	state: {
         lang:"en",
-        token:null,
+        token:localStorage.getItem('sundlfAddonToken'),
         globalLoading:false,
 	},
-    getters:{},
+    getters:{
+        getLang(state){return state.lang},
+        getToken(state){return state.token},
+        getGlobalLoading(state){return state.globalLoading},
+    },
 	mutations: {
         setLang(state,lang){
             state.lang = lang
         },
         setToken(state,token){
             state.token = token
+            localStorage.setItem('sundlfAddonToken', token);
         },
         switchGlobalLoading(state){
             state.globalLoading = !state.globalLoading
         },
+        deleteToken(state){
+            state.token = null
+            localStorage.removeItem('sundlfAddonToken');
+        }
     },
     actions: {
     },
