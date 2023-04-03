958e7118d9f01a96e5815a286043d9c0f7a60a61
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-03 09:09:13 +0900

    トークンをローカルストレージから取り出すようにした

--- src/main.js
+++ src/main.js
@@ -22,11 +22,11 @@ const vuetify = createVuetify({
 import axios from 'axios';
 window.axios = axios;
 axios.defaults.withCredentials = true;
-
 window.axios.defaults.headers.common['UserLang'] = window.navigator.language;
 axios.defaults.baseURL = 'http://127.0.0.1:8000';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('sundlfAddonToken');
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
