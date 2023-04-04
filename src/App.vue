8885c336298788c8949a339f611ee9fd40d47dbd
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-04 20:58:58 +0900

    拡張用apiを使ったログイン というかローカルストレージとか

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
     <v-app>
-        <Login v-if="this.$store.getters.getIsLogined == false "/>
+        <Login v-if="isLogined == false " @logined="logined"/>
         <Main v-else/>
-
         <!-- ここにログアウトボタン -->
         <p class="logout" @click="logout()">{{ $store.getters.getLang == 'ja'? 'ログアウト':'Log out' }}</p>
     </v-app>
@@ -12,38 +11,51 @@
 import Login from './Views/Login.vue'
 import Main from './Views/Main.vue'
 
-// Intl.DateTimeFormat().resolvedOptions().timeZone
-// window.navigator.language
-
 export default {
     data() {
         return {
+            isLogined:false
         }
     },
     components: {
         Main,
-        Login
+        Login,
     },
     methods: {
+        async removeToken(){
+            await chrome.storage.local.remove('sundlfAddonToken', function() {console.log('removed');});
+        },
         async logout(){
             this.$store.commit('switchGlobalLoading')
-            // axios通信
             await axios
                 .get('logout')
                 .then((res) => {
-                    // トークン削除
-                    localStorage.removeItem('sundlfAddonToken')
-                    this.$store.commit('setIsLogined',false)
+                    this.removeToken()
+                    // localStorage.removeItem('sundlfAddonToken')
+
+                    this.isLogined=false
                 })
                 .catch((err) => {console.log(err)})
                 .finally(()=> this.$store.commit('switchGlobalLoading',false))
+        },
+        async logined(){
+            this.isLogined=true
+            // トークンをheaderにつける
+            await chrome.storage.local.get(["sundlfAddonToken"]).then((localStrageObject) => {
+                axios.defaults.headers.common['Authorization'] = localStrageObject.sundlfAddonToken
+            })
+        },
+        async chechkLogined(){
+            await chrome.storage.local.get(["sundlfAddonToken"]).then((localStrageObject) => {
+                if (localStrageObject.sundlfAddonToken != null) {
+                    this.logined = true
+                }
+            })
         }
     },
     mounted() {
         if ((window.navigator.language).substring(0,2) == "ja") {this.$store.commit('setLang', "ja")}
-        if (localStorage.getItem('sundlfAddonToken') != null) {
-            this.$store.commit('setIsLogined',true)
-        }
+        this.chechkLogined()
     }
 }
 </script>
