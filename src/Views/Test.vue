d6e6c9b9b003573935fcff61505195f00e8fa3eb
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-04 14:18:32 +0900

    chrome.storage.localの仕様を確かめたくて作った

---
+++ src/Views/Test.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        {{ chromeData }}
+        <v-btn @click="generate()">
+            生成
+        </v-btn>
+        <v-btn @click="get()">
+            取得
+        </v-btn>
+        <v-btn @click="deleteToken()">
+            削除
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            chromeData:'none'
+        }
+    },
+    methods: {
+        async generate(){
+            await chrome.storage.local.set({ testToken : "genarated" }).then(() => {
+                console.log("seted");
+            });
+        },
+        async get(){
+            await chrome.storage.local.get(["testToken"]).then((result) => {
+                this.chromeData = result
+                // console.log("Value currently is " + result.key);
+                console.log("Value currently is " + result);
+            });
+            
+        },
+        async deleteToken(){
+            await chrome.storage.local.remove("testToken").then(()=>{
+                console.log('removed');
+            });
+        }
+    },
+}
+</script>
