14fdcbbedd125bae42e0a92b6f3cf41147f81de7
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-05 12:29:04 +0900

    今はまだ予想段階だが､vuexとかでクッキーを管理しないと行けないかも

--- src/store.js
+++ src/store.js
@@ -4,10 +4,12 @@ export default createStore({
 	state: {
         lang:"en",
         globalLoading:false,
+        cookie:"",
 	},
     getters:{
         getLang(state){return state.lang},
         getGlobalLoading(state){return state.globalLoading},
+        getCookie(state){return state.cookie}
     },
 	mutations: {
         setLang(state,lang){state.lang = lang},
@@ -15,6 +17,9 @@ export default createStore({
             if (status == null) {state.globalLoading = !state.globalLoading}
             else {state.globalLoading = status}
         },
+        setCookie(state,cookie){
+            state.cookie = cookie
+        }
     },
     actions: {
 
