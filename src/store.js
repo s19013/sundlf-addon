4e1c29b0d63be945a2b8a1c52e8d427a3a2e43b9
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-06 22:04:07 +0900

    結局使わないので削除

--- src/store.js
+++ src/store.js
@@ -4,12 +4,10 @@ export default createStore({
 	state: {
         lang:"en",
         globalLoading:false,
-        cookie:"",
 	},
     getters:{
         getLang(state){return state.lang},
         getGlobalLoading(state){return state.globalLoading},
-        getCookie(state){return state.cookie}
     },
 	mutations: {
         setLang(state,lang){state.lang = lang},
@@ -17,9 +15,6 @@ export default createStore({
             if (status == null) {state.globalLoading = !state.globalLoading}
             else {state.globalLoading = status}
         },
-        setCookie(state,cookie){
-            state.cookie = cookie
-        }
     },
     actions: {
 
